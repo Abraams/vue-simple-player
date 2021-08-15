b795fbe45a34531e6e732333bbc96b0959adeeec
Marchenko Kirill <abe.marchenko@gmail.com>
2021-08-15 03:36:36 +0300

    create duration component

---
+++ src/components/FloatingPlayer/FloatingPlayerDuration.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="floating-player__duration caption font-weight-light">
+    <span>
+      {{ getFormattedTime(curentDuration) }}
+    </span>
+    <span> / </span>
+    <span>
+      {{ getFormattedTime(totalDuration) }}
+    </span>
+  </div>
+</template>
+
+<script>
+import { timeToMs, getFormattedTime } from '@/helpers/'
+
+export default {
+  name: 'FloatingPlayerDuration',
+  props: {
+    totalDuration: {
+      type: [Number, String],
+      required: true
+    },
+    curentDuration: {
+      type: [Number, String],
+      default: 0
+    }
+  },
+  methods: {
+    getFormattedTime (time) {
+      return getFormattedTime(...timeToMs(time))
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
